3f6af1437a605b919a0520a63fbf4f47ab3cbe39
David Sypien <davidsypien@gmail.com>
2021-08-07 08:41:56 -0400

    feat: Create a book functional component

---
+++ src/Book.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import BookShelfChanger from './BookShelfChanger';
+
+const Book = (props) => {   
+   const book = props.book;
+
+   return (
+      <div className="book">
+         <div className="book-top">
+            <div className="book-cover" 
+               style={{ width: 128, height: 193, backgroundImage: (book && book.imageLinks && book.imageLinks.thumbnail) ? `url(${book.imageLinks.thumbnail})`: "" }}></div>
+            <BookShelfChanger />              
+         </div>
+         <div className="book-title">{book && book.title ? book.title : ""}</div>
+         <div className="book-authors">{book && book.authors ? book.authors.join(", "): ""}</div>
+      </div>
+   );
+}
+
+export default Book;
