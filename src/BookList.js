07ed98354f9692b49855754d1106fd3aa0aa7714
David Sypien <davidsypien@gmail.com>
2021-08-07 08:43:40 -0400

    feat: Created a book list component

---
+++ src/BookList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import BookShelf from './BookShelf';
+import { Link } from 'react-router-dom';
+
+const BookList = (props) => {
+   const shelves = props.shelves;
+
+   return (
+      <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          {shelves.map((s)=> (
+            <BookShelf key={s.id} shelf={s} />
+          ))}                             
+        </div>
+      </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+   );
+}
+
+export default BookList;
